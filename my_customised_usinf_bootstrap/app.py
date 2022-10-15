a6392171a8bc85a9ab24358dc57fd72338acafb6
ssubbu07 <98497915+ssubbu07@users.noreply.github.com>
2022-10-15 16:34:29 -0500

    Update app.py

--- my_customised_usinf_bootstrap/app.py
+++ my_customised_usinf_bootstrap/app.py
@@ -1,67 +1,87 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import cv2
-import numpy as np 
-from tensorflow.keras.models import load_model
+from keras.models import load_model
+import numpy as np
+from camera import VideoCamera
 
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
+video = cv2.VideoCapture(0)
+
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-	image = request.files['select_file']
+# def index():
+#     return render_template('index.html')
 
-	image.save('static/file.jpg')
-
-	image = cv2.imread('static/file.jpg')
-
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-	
-	faces = cascade.detectMultiScale(gray, 1.1, 3)
-
-	for x,y,w,h in faces:
-		cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
-
-		cropped = image[y:y+h, x:x+w]
+def gen(video):
+    while True:
+        # success, image = video.read()
+        frame = video.get_frame()
+        # ret, jpeg = cv2.imencode('.jpg', image)
+        # frame = video.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-	cv2.imwrite('static/after.jpg', image)
-	try:
-		cv2.imwrite('static/cropped.jpg', cropped)
+@app.route('/after', methods=['GET', 'POST'])
+def after():
+    img = request.files['file1']
 
-	except:
-		pass
+    img.save('static/file.jpg')
 
+    ####################################
+    img1 = cv2.imread('static/file.jpg')
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    faces = cascade.detectMultiScale(gray, 1.1, 3)
 
+    for x,y,w,h in faces:
+        cv2.rectangle(img1, (x,y), (x+w, y+h), (0,255,0), 2)
 
-	try:
-		img = cv2.imread('static/cropped.jpg', 0)
+        cropped = img1[y:y+h, x:x+w]
 
-	except:
-		img = cv2.imread('static/file.jpg', 0)
+    cv2.imwrite('static/after.jpg', img1)
 
-	img = cv2.resize(img, (48,48))
-	img = img/255
+    try:
+        cv2.imwrite('static/cropped.jpg', cropped)
 
-	img = img.reshape(1,48,48,1)
+    except:
+        pass
 
-	model = load_model('model_3.h5')
+    #####################################
 
-	pred = model.predict(img)
+    try:
+        image = cv2.imread('static/cropped.jpg', 0)
+    except:
+        image = cv2.imread('static/file.jpg', 0)
 
+    image = cv2.resize(image, (48,48))
 
-	label_map = ['Anger','Neutral' , 'Fear', 'Happy', 'Sad', 'Surprise']
-	pred = np.argmax(pred)
-	final_pred = label_map[pred]
+    image = image/255.0
 
+    image = np.reshape(image, (1,48,48,1))
 
-	return render_template('predict.html', data=final_pred)
+    model = load_model('model.h5')
 
+    prediction = model.predict(image)
+
+    label_map =   ['Anger','Neutral' , 'Fear', 'Happy', 'Sad', 'Surprise']
+
+    prediction = np.argmax(prediction)
+
+    final_prediction = label_map[prediction]
+
+    return render_template('after.html', data=final_prediction)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
